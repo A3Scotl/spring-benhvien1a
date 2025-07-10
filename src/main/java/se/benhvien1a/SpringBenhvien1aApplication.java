26bbaf8ab98a324b68fbcad37eec9e27f68797fd
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-07-11 00:28:52 +0700

    init and setup  project

---
+++ src/main/java/se/benhvien1a/SpringBenhvien1aApplication.java
@@ -0,0 +1,13 @@
+package se.benhvien1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBenhvien1aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBenhvien1aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/benhvien1a/SpringBenhvien1aApplicationTests.java
@@ -0,0 +1,13 @@
+package se.benhvien1a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBenhvien1aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
